8767c5a7a077ee519f96e5269563e4494aff28f2
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-02-01 17:31:30 +0100

    Fine Eercizio Mail

--- e-mail/js/script.js
+++ e-mail/js/script.js
@@ -10,14 +10,16 @@ stampa un messaggio appropriato sull’esito del controllo.
 const mails = ["gianmarco@hotmail.com","pimentel@hotmail.com","gianmarco@gmail.com", "pimentel@gmail.com","gianmarco@libero.it", "pimentel@libero.it",];
 console.log(mails);
 
-
+//memorizzo il button su JS
 const buttonElement = document.querySelector("#searchButton");
-
+// dico al computer le azione ceh deve eseguire dopo il click
 buttonElement.addEventListener("click",
 
     function(){
             // chiedere all'utente di inserire una mail
             const mailUtente = document.getElementById("mail").value;
+
+            // verificare se la mail inseirta dall'utente è presente delle mail contenute negli array
             let y = 0 ;
             
             for(let i=0; i<mails.length; i++){
@@ -27,6 +29,7 @@ buttonElement.addEventListener("click",
                 } 
             }
 
+            // stamparea a schermo il risultato
             if (y == 0){
                 document.getElementById("result").innerText =`La mail inserita non è corretta.`;
             } else{
@@ -39,19 +42,5 @@ buttonElement.addEventListener("click",
 )
 
 
-// verificare se la mail inseirta dall'utente è presente delle mail contenute negli array
 
-// stamparea a schermo il risultato
 
-/* 
-function in_array(mailUtente, mails) {
-                for(i=0; i<mails.length;i++){
-                    if(mailUtente == mails[i]){
-                        return true;
-                        document.querySelector("result").innerHTML=`${mailUtente}`;
-                    }
-                        return false;
-                        document.querySelector("result").innerHTML= `La mail è inesitente`;
-                }
-            }
-*/
