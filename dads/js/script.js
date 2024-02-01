78d364fb7016ab20eb8e80850c415907106d8f7b
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-02-01 17:13:05 +0100

    correzzione esercizio dadi

--- dads/js/script.js
+++ dads/js/script.js
@@ -4,14 +4,35 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
-// devo far scegliere un numero all'utente
-const user = Math.floor(Math.random() * 6 + 1);
-console.log(user);
 
-// devo far scegliere un numero al computer
-const computer = Math.floor(Math.random() * 6 + 1);
-console.log(computer);
 
+const buttonElement = document.getElementById("buttonplay");
+
+buttonElement.addEventListener("click",
+        function(){
+            // devo far scegliere un numero all'utente
+            const inputElement = document.getElementById("userChoice").value;
+             console.log(inputElement);
+
+
+             // devo far scegliere un numero al computer
+            const computer = Math.floor(Math.random() * 6 + 1);
+            console.log(computer);      
+            if(inputElement>computer){
+                document.getElementById("result").innerHTML = `Il numero ${inputElement} scelto dall'utente è maggiore del numero ${computer} scelto dal computer.<br>Hai vinto!`
+            } else if (inputElement<computer){
+                document.getElementById("result").innerHTML = `Il numero ${inputElement} scelto dall'utente è minore del numero ${computer} scelto dal computer.<br>Hai perso!`
+            
+            } else{
+                document.getElementById("result").innerHTML = `Il numero ${inputElement} scelto dall'utente e il numero ${computer} scelto dal computer sono uguali.<br>Hai pareggiato!`
+            
+            }
+            
+    }
+)
+
+
+// controllo che il valore inserito sia tra 1 e 6
 /* // crerare una condizione per la quale vince sempre il numero più alto
 
 const buttonGameElement = document.querySelector("#formButton");
@@ -34,15 +55,6 @@ buttonGameElement.addEventListener("click",
 
 
 
-if(user>computer){
-    document.getElementById("result").innerHTML = `Il numero ${user} scelto dall'utente è maggiore del numero ${computer} scelto dal computer. Hai vinto!`
-} else if (user<computer){
-    document.getElementById("result").innerHTML = `Il numero ${user} scelto dall'utente è minore del numero ${computer} scelto dal computer. Hai perso!`
-
-} else{
-    document.getElementById("result").innerHTML = `Il numero ${user} scelto dall'utente e il numero ${computer} scelto dal computer sono uguali. Hai pareggiato!`
-
-}
 
 
 
