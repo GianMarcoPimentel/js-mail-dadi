523a873c1adaf1bb0a7b078864047dc403c1261d
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-02-01 17:35:29 +0100

    Fine Esercizio Dads

--- dads/js/script.js
+++ dads/js/script.js
@@ -11,24 +11,24 @@ const buttonElement = document.getElementById("buttonplay");
 buttonElement.addEventListener("click",
         function(){
             // devo far scegliere un numero all'utente
-            const inputElement = document.getElementById("userChoice").value;
-             console.log(inputElement);
+            const user = Math.floor(Math.random() * 6 + 1);
+            /* console.log(user); */
 
 
-             // devo far scegliere un numero al computer
+            // devo far scegliere un numero al computer
             const computer = Math.floor(Math.random() * 6 + 1);
-            console.log(computer); 
+            /* console.log(computer);  */
             
-            // devo controllare che il numerom inserito sia effettivamente un numero, e che sia compreso tra 1 e 6!
+          /*   // devo controllare che il numerom inserito sia effettivamente un numero, e che sia compreso tra 1 e 6!
+            if() */
 
-            
-            if(inputElement>computer){
-                document.getElementById("result").innerHTML = `Il numero ${inputElement} scelto dall'utente è maggiore del numero ${computer} scelto dal computer.<br>Hai vinto!`
-            } else if (inputElement<computer){
-                document.getElementById("result").innerHTML = `Il numero ${inputElement} scelto dall'utente è minore del numero ${computer} scelto dal computer.<br>Hai perso!`
+            if(user>computer){
+                document.getElementById("result").innerHTML = `Il numero ${user} scelto dall'utente è maggiore del numero ${computer} scelto dal computer.<br>Hai vinto!`
+            } else if (user<computer){
+                document.getElementById("result").innerHTML = `Il numero ${user} scelto dall'utente è minore del numero ${computer} scelto dal computer.<br>Hai perso!`
             
             } else{
-                document.getElementById("result").innerHTML = `Il numero ${inputElement} scelto dall'utente e il numero ${computer} scelto dal computer sono uguali.<br>Hai pareggiato!`
+                document.getElementById("result").innerHTML = `Il numero ${user} scelto dall'utente e il numero ${computer} scelto dal computer sono uguali.<br>Hai pareggiato!`
             
             }
             
@@ -37,25 +37,8 @@ buttonElement.addEventListener("click",
 
 
 // controllo che il valore inserito sia tra 1 e 6
-/* // crerare una condizione per la quale vince sempre il numero più alto
+// crerare una condizione per la quale vince sempre il numero più alto
 
-const buttonGameElement = document.querySelector("#formButton");
-
-buttonGameElement.addEventListener("click",
-
-    function(){
-        if(user>computer){
-            document.getElementById("result").innerHTML = `Il numero ${user} scelto dall'utente è maggiore del numero ${computer} scelto dal computer. Hai vinto!`
-        } else if (user<computer){
-            document.getElementById("result").innerHTML = `Il numero ${user} scelto dall'utente è minore del numero ${computer} scelto dal computer. Hai perso!`
-  
-        } else{
-            document.getElementById("result").innerHTML = `Il numero ${user} scelto dall'utente e il numero ${computer} scelto dal computer sono uguali. Hai pareggiato!`
-
-        }
-    }
-
-) */
 
 
 
