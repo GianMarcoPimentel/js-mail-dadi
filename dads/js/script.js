ac77638685a4823c068852e0e82441cb195582b2
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-02-01 15:31:34 +0100

    fine esercizio dadi

--- dads/js/script.js
+++ dads/js/script.js
@@ -5,5 +5,51 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
 // devo far scegliere un numero all'utente
+const user = Math.floor(Math.random() * 6 + 1);
+console.log(user);
+
 // devo far scegliere un numero al computer
-// crerare una condizione per la quale vince sempre il numero più alto
+const computer = Math.floor(Math.random() * 6 + 1);
+console.log(computer);
+
+/* // crerare una condizione per la quale vince sempre il numero più alto
+
+const buttonGameElement = document.querySelector("#formButton");
+
+buttonGameElement.addEventListener("click",
+
+    function(){
+        if(user>computer){
+            document.getElementById("result").innerHTML = `Il numero ${user} scelto dall'utente è maggiore del numero ${computer} scelto dal computer. Hai vinto!`
+        } else if (user<computer){
+            document.getElementById("result").innerHTML = `Il numero ${user} scelto dall'utente è minore del numero ${computer} scelto dal computer. Hai perso!`
+  
+        } else{
+            document.getElementById("result").innerHTML = `Il numero ${user} scelto dall'utente e il numero ${computer} scelto dal computer sono uguali. Hai pareggiato!`
+
+        }
+    }
+
+) */
+
+
+
+if(user>computer){
+    document.getElementById("result").innerHTML = `Il numero ${user} scelto dall'utente è maggiore del numero ${computer} scelto dal computer. Hai vinto!`
+} else if (user<computer){
+    document.getElementById("result").innerHTML = `Il numero ${user} scelto dall'utente è minore del numero ${computer} scelto dal computer. Hai perso!`
+
+} else{
+    document.getElementById("result").innerHTML = `Il numero ${user} scelto dall'utente e il numero ${computer} scelto dal computer sono uguali. Hai pareggiato!`
+
+}
+
+
+
+
+        
+
+
+
+
+
