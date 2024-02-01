8deea1a551816792e8989255fff8c57fe75ad3f7
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-02-01 14:35:19 +0100

    first commit

---
+++ dads/js/script.js
@@ -0,0 +1,9 @@
+/* 
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
+
+// devo far scegliere un numero all'utente
+// devo far scegliere un numero al computer
+// crerare una condizione per la quale vince sempre il numero più alto
